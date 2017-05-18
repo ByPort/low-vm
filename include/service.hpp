2bf506452ebc5922227fb44700eb209372e6c6ce
Vladimir Sernatsky <byport1112@gmail.com>
2017-05-18 20:47:02 +0300

    dev: events support in Service class
    #intermediate

--- include/service.hpp
+++ include/service.hpp
@@ -1,13 +1,18 @@
-#ifndef SERVICE_H
-#define SERVICE_H
+#ifndef INCLUDE_SERVICE_HPP_
+#define INCLUDE_SERVICE_HPP_
 
 #include <isa.hpp>
+#include <vm.hpp>
 
 namespace lowvm {
 class Service {
  public:
-  virtual void operator()(addr service_header) = 0;
+  virtual void interrupt(lowvm::VM* context, addr service_header) = 0;
+  virtual void attach(lowvm::VM* context) = 0;
+  virtual void stepOn(lowvm::VM* context) = 0;
+  virtual void stepOff(lowvm::VM* context) = 0;
+  virtual void halt(lowvm::VM* context) = 0;
 };
 }  // namespace lowvm
 
-#endif
+#endif  // INCLUDE_SERVICE_HPP_
